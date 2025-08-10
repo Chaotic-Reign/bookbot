efc9caefaefd4accc5c277982802a2b166497705
Chaotic_Reign <noreply@notreal.com>
2025-08-09 19:50:57 -0500

    working on new function in stats

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 from stats import get_num_words
+from stats import count_characters
 
-get_num_words()
+get_num_words("books/frankenstein.txt")
+
+def get_character_counts():
+    result = count_characters("books/frankenstein.txt")
+    for value in result:
+        count = result[value]
+        print(f"character: {value}, count: {count}")
--- stats.py
+++ stats.py
@@ -3,10 +3,24 @@ def get_book_text(path_to_file):
         file_contents = f.read()
         return file_contents
 
-def get_num_words():
-    result = get_book_text("books/frankenstein.txt")
+def get_num_words(path_to_file):
+    result = get_book_text(path_to_file)
     words = result.split()
     num_words = 0
     for word in words:
         num_words += 1
     print(f"{num_words} words found in the document")
+
+def count_characters(path_to_file):
+    text = get_book_text(path_to_file)
+    text = text.lower()
+    words = text.split()
+    characters = {}
+    for word in words:
+        for char in word:
+            if f"{char}" in characters:
+                char += 1
+            else:
+                characters[{char}] = 1
+    return characters
+
