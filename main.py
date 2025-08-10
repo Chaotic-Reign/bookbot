9a2ab15b539956509af387391604fc45b96a8b06
Chaotic_Reign <noreply@notreal.com>
2025-08-09 20:49:29 -0500

    updated stats and main

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from stats import get_num_words
 from stats import count_characters
 
-get_num_words("books/frankenstein.txt")
-
-def get_character_counts():
-    result = count_characters("books/frankenstein.txt")
-    for value in result:
-        count = result[value]
-        print(f"character: {value}, count: {count}")
+word_count = get_num_words()
+characters = count_characters()
+print(f"{word_count} words found in the document")
+for char in characters:
+    count = characters[char]
+    print(f"'{char}': {count}")
--- stats.py
+++ stats.py
@@ -3,24 +3,19 @@ def get_book_text(path_to_file):
         file_contents = f.read()
         return file_contents
 
-def get_num_words(path_to_file):
-    result = get_book_text(path_to_file)
-    words = result.split()
-    num_words = 0
-    for word in words:
-        num_words += 1
-    print(f"{num_words} words found in the document")
+def get_num_words():
+    words = get_book_text("books/frankenstein.txt")
+    words = words.split()
+    return len(words)
 
-def count_characters(path_to_file):
-    text = get_book_text(path_to_file)
+def count_characters():
+    text = get_book_text("books/frankenstein.txt")
     text = text.lower()
-    words = text.split()
     characters = {}
-    for word in words:
-        for char in word:
-            if f"{char}" in characters:
-                char += 1
-            else:
-                characters[{char}] = 1
+    for char in text:
+        if char in characters:
+            characters[char] += 1
+        else:
+            characters[char] = 1
     return characters
 
